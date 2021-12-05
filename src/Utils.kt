a70c25445451a3b266553919ff02e94dfa5df1d8
Rob Meeuwisse <rob.meeuwisse@xs4all.nl>
2021-12-05 15:33:47 +0100

    Add solution template

--- src/Utils.kt
+++ src/Utils.kt
@@ -1,13 +1,15 @@
-import java.io.File
 import java.math.BigInteger
 import java.security.MessageDigest
+import kotlin.io.path.Path
+import kotlin.io.path.readLines
 
-/**
- * Reads lines from the given input txt file.
- */
-fun readInput(name: String) = File("src", "$name.txt").readLines()
+/** Return the class of the caller */
+inline val callerClass: Class<*>
+    get() = Class.forName(Throwable().stackTrace.first().className)
 
-/**
- * Converts string to md5 hash.
- */
+/** Read lines from the given file */
+@Suppress("NOTHING_TO_INLINE") // Inlining to stay in caller's stack frame
+inline fun readInput(fileName: String) = Path("src", callerClass.packageName, fileName).readLines()
+
+/** Convert string to md5 hash */
 fun String.md5(): String = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray())).toString(16)
---
+++ src/day__/Solution.kt
@@ -0,0 +1,30 @@
+package day__ // TODO: Correct the package name
+
+import readInput
+
+/**
+ * TODO: Update the title and link
+ * [$TITLE](https://adventofcode.com/2021/day/$DAY)
+ */
+fun main() {
+    fun part1(input: List<String>): Int {
+        val result = 0
+        println("Part 1 answer: $result")
+        return result
+    }
+
+    fun part2(input: List<String>): Int {
+        val result = 0
+        println("Part 2 answer: $result")
+        return result
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("input_test.txt")
+    check(part1(testInput) == 0)
+    check(part2(testInput) == 0)
+
+    val input = readInput("input.txt")
+    check(part1(input) == 0)
+    check(part2(input) == 0)
+}
